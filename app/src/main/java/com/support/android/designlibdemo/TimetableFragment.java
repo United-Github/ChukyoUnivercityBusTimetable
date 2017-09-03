fc23134c19e25a3ea777ad032abeba907d3514f4
ragrog <ragrog216@outlook.jp>
2017-09-03 15:21:43 +0900

    休日の場合に、運休の旨を表示する機能を実装
    直近のバスが無い場合の表示機能の関数を実装

--- app/src/main/java/com/support/android/designlibdemo/TimetableFragment.java
+++ app/src/main/java/com/support/android/designlibdemo/TimetableFragment.java
@@ -17,75 +17,179 @@
 package com.support.android.designlibdemo;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.TextViewCompat;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.support.android.designlibdemo.TimeManager.TimeManager;
+import com.support.android.designlibdemo.TimetableList.layout.BusTimeListHeaderViewManager;
 import com.support.android.designlibdemo.TimetableList.layout.BusTimeListViewManager;
-import com.support.android.designlibdemo.TimetableList.layout.OnBusTimeItemClickListener;
 import com.support.android.designlibdemo.TimetableList.layout.TimeListCustomAdapter;
 import com.support.android.designlibdemo.TimetableList.model.TimeItemModel;
-import com.support.android.designlibdemo.TimetableList.model.TimeMinutesListItemModel;
 
-import java.sql.Time;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.LogRecord;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+
 @SuppressLint("ValidFragment")
 public class TimetableFragment extends Fragment {
-    private BusTimeListViewManager busTimeListViewManager;
+    private BusTimeListViewManager listViewManager;
+    private BusTimeListHeaderViewManager headerViewManager;
     private TimeListCustomAdapter adapter;
     private final boolean isDepartJosui;
     private final int depart;
     private TimeManager timeManager;
+    private int currentBusTimeMonth;
+    private int currentBusTimeDate;
+    private int currentBusTimeHour;
+    private int currentBusTimeMinutes;
+    private Calendar currentBusTimeCalendar;
+    private long remainingMillis;
+    private Handler handler = new Handler();
+    private final static int updateInterval = 100;
+    private Timer timer;
+
     public TimetableFragment(boolean isDepartJosui){
         super();
         this.isDepartJosui = isDepartJosui;
         this.depart = (isDepartJosui)? TimeManager.DEPART_JOSUI : TimeManager.DEPART_UNIVERCITY;
     }
-    @BindView(R.id.time_list_scrollview)
-    ScrollView scrollView;
+
+//    @BindView(R.id.time_list_scrollview)
+//    ScrollView scrollView;
+    @BindView(R.id.bus_time_suspended)
+    TextView mSuspendedText;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rv = inflater.inflate(R.layout.fragment_cheese_list, container, false);
-        busTimeListViewManager = new BusTimeListViewManager(rv.findViewById(R.id.time_list_scrollview));
+        ButterKnife.bind(this, rv);
+        listViewManager = new BusTimeListViewManager(rv.findViewById(R.id.time_list_scrollview));
+        headerViewManager = new BusTimeListHeaderViewManager(rv.findViewById(R.id.time_header_root));
+        headerViewManager.setOnNextClickListener(new onClickeHeaderViewButtonListener(true));
+        headerViewManager.setOnPreviousClickListener(new onClickeHeaderViewButtonListener(false));
         adapter = new TimeListCustomAdapter(getContext());
         timeManager = ((BusTimerApplication)getActivity().getApplication()).getInstanceTimeManager();
-        setTestData();
+        timer = new Timer();
+        setCurrentDay();
         return rv;
     }
 
-
-    private void setTestData(){
+    private void setCurrentDay(){
         Calendar calendar = Calendar.getInstance();
-        int month = calendar.get(Calendar.MONTH) + 1;
-        int date = calendar.get(Calendar.DATE);
+        currentBusTimeMonth = calendar.get(Calendar.MONTH) + 1;
+        currentBusTimeDate = calendar.get(Calendar.DATE);
         try {
-            List<TimeItemModel> itemModels = timeManager.getBusSchedule(month, date, depart);
+            List<TimeItemModel> itemModels = timeManager.getBusSchedule(currentBusTimeMonth, currentBusTimeDate, depart);
             for (final TimeItemModel item : itemModels){
-                busTimeListViewManager.addTimeItemModel(item);
+                listViewManager.addTimeItemModel(item);
             }
+            timer.schedule(new updateRemainingTask(), 0, updateInterval);
         } catch (TimeManager.NoScheduleException e) {
+//            headerViewManager.setNonBusTime();
+            headerViewManager.setSuspended();
+//            scrollView.setVisibility(View.GONE);
+            mSuspendedText.setVisibility(View.VISIBLE);
             e.printStackTrace();
+        }
+    }
+
+    private void setHeaderViewButtonLitener(){
+    }
+
+    private void updateCurrentBusTime(Calendar current){
+        current = Calendar.getInstance();
+        try {
+            int currentBusTime[] = timeManager.nearBusTime(
+                    currentBusTimeMonth,
+                    currentBusTimeDate,
+                    current.get(Calendar.HOUR_OF_DAY),
+                    current.get(Calendar.MINUTE),
+                    depart);
+            currentBusTimeHour = currentBusTime[0];
+            currentBusTimeMinutes = currentBusTime[1];
+            currentBusTimeCalendar = Calendar.getInstance();
+            currentBusTimeCalendar.set(timeManager.YEAR, currentBusTimeMonth -1, currentBusTimeDate, currentBusTimeHour, currentBusTimeMinutes);
+            remainingMillis = currentBusTimeCalendar.getTimeInMillis() - Calendar.getInstance().getTimeInMillis();
+            headerViewManager.setDepartTime(currentBusTimeHour, currentBusTimeMinutes);
+        } catch (TimeManager.NoScheduleException | TimeManager.DayOverflowException e) {
+            e.printStackTrace();
+        }
+
+    }
+    class updateRemainingTask extends TimerTask {
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    remainingMillis -= updateInterval;
+                    if (remainingMillis <= 0){
+                        updateCurrentBusTime(Calendar.getInstance());
+                    } else {
+                        long remainingSeconds = remainingMillis / 1000;
+                        int minutes = (int)(remainingSeconds / 60);
+                        int seconds = (int)(remainingSeconds % 60);
+                        headerViewManager.setRemainingTime(minutes, seconds);;
+                        listViewManager.setCurrentTime(currentBusTimeHour, currentBusTimeMinutes);
+                    }
+                }
+            });
+        }
+    }
+
+    private class onClickeHeaderViewButtonListener implements View.OnClickListener{
+        private boolean next;
+        onClickeHeaderViewButtonListener(boolean next){
+            this.next = next;
+        }
+        @Override
+        public void onClick(View view) {
+            try {
+                int busTime[];
+                if (next){
+                    busTime = timeManager.afterBusTime(
+                            currentBusTimeMonth,
+                            currentBusTimeDate,
+                            currentBusTimeHour,
+                            currentBusTimeMinutes,
+                            depart);
+                } else {
+                    busTime = timeManager.beforeBusTime(
+                            currentBusTimeMonth,
+                            currentBusTimeDate,
+                            currentBusTimeHour,
+                            currentBusTimeMinutes,
+                            depart);
+                }
+                Calendar current = Calendar.getInstance();
+                current.set(
+                        timeManager.YEAR,
+                        currentBusTimeMonth,
+                        currentBusTimeDate,
+                        busTime[0],
+                        busTime[1]);
+                updateCurrentBusTime(current);
+            } catch (TimeManager.NoScheduleException e) {
+                e.printStackTrace();
+            } catch (TimeManager.DayOverflowException e) {
+                e.printStackTrace();
+            } catch (TimeManager.DayUnderflowException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- app/src/main/java/com/support/android/designlibdemo/TimetableList/layout/BusTimeListHeaderViewManager.java
+++ app/src/main/java/com/support/android/designlibdemo/TimetableList/layout/BusTimeListHeaderViewManager.java
@@ -2,7 +2,8 @@ package com.support.android.designlibdemo.TimetableList.layout;
 
 import android.content.Context;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.support.android.designlibdemo.R;
@@ -18,10 +19,13 @@ public class BusTimeListHeaderViewManager {
     private TextView mSecondsText;
     private TextView mScheduleTypeText;
     private TextView mDepartTimeText;
-    private Button mNextButton;
-    private Button mPreviousButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreviousButton;
     private Context mContext;
     private ScheduleType mScheduleType;
+    private LinearLayout mRootView;
+    private LinearLayout mHeaderInfo;
+    private TextView mNonInfo;
 
     public BusTimeListHeaderViewManager(View rootView){
         bind(rootView);
@@ -34,14 +38,32 @@ public class BusTimeListHeaderViewManager {
         mSecondsText = (TextView)rootView.findViewById(R.id.time_header_remaining_seconds);
         mScheduleTypeText = (TextView)rootView.findViewById(R.id.time_header_schedule_type);
         mDepartTimeText = (TextView)rootView.findViewById(R.id.time_header_depart);
-        mNextButton = (Button)rootView.findViewById(R.id.time_header_next);
-        mPreviousButton = (Button)rootView.findViewById(R.id.time_header_previous);
+        mNextButton = (ImageButton) rootView.findViewById(R.id.time_header_next);
+        mPreviousButton = (ImageButton) rootView.findViewById(R.id.time_header_previous);
+        mRootView = (LinearLayout)rootView.findViewById(R.id.time_header_root);
+        mHeaderInfo = (LinearLayout)rootView.findViewById(R.id.time_header_info);
+        mNonInfo = (TextView) rootView.findViewById(R.id.time_header_non);
     }
     // バス発車時刻の設定
     public void setDepartTime(int hour, int minutes){
         mDepartTimeText.setText(mContext.getString(R.string.time_list_header_depart, hour, minutes));
-        mMinutesText.setText(Integer.toString(hour));
-        mSecondsText.setText(Integer.toString(minutes));
+    }
+    // 残り時間を設定
+    public void setRemainingTime(int hour, int minutes){
+        mMinutesText.setText(mContext.getString(R.string.time_list_header_time, hour));
+        mSecondsText.setText(mContext.getString(R.string.time_list_header_time, minutes));
+    }
+    // バスが運休のとき
+    public void setSuspended(){
+        mRootView.setVisibility(View.GONE);
+    }
+    // バスの時刻が存在しないとき
+    public void setNonBusTime(){
+        mNextButton.setVisibility(View.GONE);
+        mPreviousButton.setVisibility(View.GONE);
+        mHeaderInfo.setVisibility(View.GONE);
+        mNonInfo.setVisibility(View.VISIBLE);
+        mNonInfo.setText(mContext.getString(R.string.time_list_header_finished));
     }
     // ダイヤを設定
     public void setScheduleType(ScheduleType type) {
